b3c0cc9cf5b44c3b356300caf29b5578cb47a34f
Jesus Quintana <jesquiliche@hotmail.com>
2022-05-11 13:33:21 +0200

    implemetacion jwt 2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,11 +53,16 @@ class AuthController extends Controller
     }
 
     public function register(Request $request){
-        $request->validate([
+ 
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
+        if($validator->fails()){
+            return response()->json($validator->errors(),422); 
+        }
+ 
 
         $user = User::create([
             'name' => $request->name,
